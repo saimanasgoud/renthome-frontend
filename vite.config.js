61d5ffe76691a4cbc7fc96e5b89fb778ef135853
sai manas goud <saimanas670@gmail.com>
2026-04-04 19:35:36 +0530

    fixed signup casing issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/renthome',
+  base: '/',
   plugins: [react()],
   server: {
     host: true,
